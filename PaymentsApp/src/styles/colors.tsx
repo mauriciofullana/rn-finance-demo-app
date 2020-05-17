120432553f622c194c5f2e81635305fb288f662c
Mauricio Fullana <mauriciof@bankingly.com>
2020-05-17 20:49:58 -0300

    Initial global styles files added.

---
+++ PaymentsApp/src/styles/colors.tsx
@@ -0,0 +1,24 @@
+export const black = '#000000';
+export const darkestGray = '#29292c';
+export const darkGray = '#333';
+export const darkPurple = '#282b36';
+export const mediumGray = '#9b9aa1';
+export const lightGray = '#DBDBDB';
+export const lightWarmGray = '#f5f5f5';
+export const offWhite = '#b7bdc5';
+export const tan = '#fcf8f2';
+export const thoughtbotRed = '#ed3e44';
+export const transparent = 'rgba(0, 0, 0, 0)';
+export const white = '#ffffff';
+
+export const main = '#f4351e';
+export const secondary = '#f4351e';
+export const header = '#030303';
+
+export const baseText = darkGray;
+export const darkText = darkestGray;
+export const sectionBackground = lightWarmGray;
+export const background = white;
+export const selected = thoughtbotRed;
+export const unselected = lightGray;
+export const headerInactiveIcon = black;
