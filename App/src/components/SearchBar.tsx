8295337d5ae30c3a0695cd8693931b46793e4400
Mauricio Fullana <mauricio@bankingly.com>
2020-09-10 00:58:22 -0300

    SearchBar styling change.

--- App/src/components/SearchBar.tsx
+++ App/src/components/SearchBar.tsx
@@ -4,62 +4,63 @@ import { Feather } from '@expo/vector-icons';
 import { Colors } from '../styles';
 
 export interface SearchBarProps {
-    term: string,
-    onTermChange: any
+	term: string;
+	onTermChange: any;
 }
 
-const SearchBar: FunctionComponent<SearchBarProps> = ({ term, onTermChange }) => {
+const SearchBar: FunctionComponent<SearchBarProps> = ({
+	term,
+	onTermChange,
+}) => {
+	const clearTerm = () => {
+		onTermChange('');
+	};
 
-    const clearTerm = () => {
-        onTermChange('');
-    }
-
-    return <View style={styles.backgroundView}>
-        <Feather style={styles.searchIcon}
-            name="search"
-        />
-        <TextInput
-            style={styles.input}
-            placeholder="Buscar"
-            placeholderTextColor={Colors.mediumGray}
-            autoCapitalize="none"
-            autoCorrect={false}
-            value={term}
-            onChangeText={onTermChange}
-        />
-        {!!term && <TouchableOpacity style={styles.clearIconContainer} onPress={clearTerm}>
-                <Feather style={styles.searchIcon}
-                    name="x-circle"
-                />
-            </TouchableOpacity>
-        }
-    </View>
-}
+	return (
+		<View style={styles.backgroundView}>
+			<Feather style={styles.searchIcon} name="search" />
+			<TextInput
+				style={styles.input}
+				placeholder="Buscar"
+				placeholderTextColor={Colors.mediumGray}
+				autoCapitalize="none"
+				autoCorrect={false}
+				value={term}
+				onChangeText={onTermChange}
+			/>
+			{!!term && (
+				<TouchableOpacity style={styles.clearIconContainer} onPress={clearTerm}>
+					<Feather style={styles.searchIcon} name="x-circle" />
+				</TouchableOpacity>
+			)}
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    backgroundView: {
-        marginTop: 20,
-        marginBottom: 20,
-        flexDirection: 'row',
-        height: 40,
-        borderColor: Colors.mediumGray,
-        borderWidth: 0.5,
-        borderRadius: 5
-    },
-    searchIcon: {
-        color: Colors.mediumGray,
-        marginHorizontal: 15,
-        fontSize: 24,
-        alignSelf: "center"
-    },
-    clearIconContainer: {
-        alignSelf: "center"
-    },
-    input: {
-        flex: 1,
-        fontSize: 16,
-        color: Colors.lightGray
-    }
+	backgroundView: {
+		marginTop: 20,
+		marginBottom: 20,
+		flexDirection: 'row',
+		height: 35,
+		borderColor: Colors.mediumGray,
+		borderWidth: 0.5,
+		borderRadius: 5,
+	},
+	searchIcon: {
+		color: Colors.mediumGray,
+		marginHorizontal: 15,
+		fontSize: 22,
+		alignSelf: 'center',
+	},
+	clearIconContainer: {
+		alignSelf: 'center',
+	},
+	input: {
+		flex: 1,
+		fontSize: 15,
+		color: Colors.lightGray,
+	},
 });
 
 export default SearchBar;
