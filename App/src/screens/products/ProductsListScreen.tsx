c947cb1babf79fdaf4f3e70e65c1258be8fb719c
Mauricio Fullana <mauricio@bankingly.com>
2020-09-13 00:32:15 -0300

    Products screen - add product type filter modal (not implemented)

--- App/src/screens/products/ProductsListScreen.tsx
+++ App/src/screens/products/ProductsListScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FunctionComponent } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { useSelector } from 'react-redux';
 import {
 	Foundation,
@@ -7,15 +7,19 @@ import {
 	Ionicons,
 	MaterialCommunityIcons,
 } from '@expo/vector-icons';
+import Modal from 'react-native-modal';
+import CheckBox from 'react-native-check-box';
 
 import SearchBar from '../../components/SearchBar';
-import { Colors } from '../../styles';
+import { Colors, Buttons } from '../../styles';
 import { productsSelector } from '../../state/selectors';
 import { IProduct } from '../../state/products/types';
 import { baseFontSize, largeFontSize } from '../../styles/typography';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { transparent } from '../../styles/colors';
 
 const ProductsListScreen: FunctionComponent = () => {
+	const [modalVisible, setModalVisible] = useState(false);
 	const [refreshing, setRefreshing] = useState(false);
 	const { products } = useSelector(productsSelector);
 	const [term, setTerm] = useState('');
@@ -33,12 +37,13 @@ const ProductsListScreen: FunctionComponent = () => {
 					<View
 						style={{
 							width: 20,
+							paddingRight: 0,
 							alignItems: 'center',
 						}}
 					>
 						{renderIcon(product.productType)}
 					</View>
-					<View style={{ marginLeft: 10, flex: 1 }}>
+					<View style={{ flex: 1, marginLeft: 10 }}>
 						<Text style={{ color: Colors.baseText, marginBottom: 3 }}>
 							{product.productType}
 						</Text>
@@ -99,7 +104,12 @@ const ProductsListScreen: FunctionComponent = () => {
 				<View style={{ flex: 1, alignItems: 'center' }}>
 					<SearchBar term={term} onTermChange={setTerm} />
 				</View>
-				<TouchableOpacity style={{ marginLeft: 10 }}>
+				<TouchableOpacity
+					style={{ marginLeft: 10 }}
+					onPress={() => {
+						setModalVisible((prevState) => !prevState);
+					}}
+				>
 					<MaterialCommunityIcons
 						name="filter-variant"
 						size={35}
@@ -116,6 +126,100 @@ const ProductsListScreen: FunctionComponent = () => {
 				ItemSeparatorComponent={renderSeparator}
 				showsVerticalScrollIndicator={false}
 			/>
+			<Modal isVisible={modalVisible} backdropOpacity={0.6}>
+				<View
+					style={{
+						marginHorizontal: 20,
+						marginVertical: 30,
+						backgroundColor: Colors.screenBackground,
+						borderRadius: 10,
+					}}
+				>
+					<View
+						style={{
+							height: 50,
+							width: '100%',
+							backgroundColor: Colors.screenBackground,
+							borderTopRightRadius: 10,
+							borderTopLeftRadius: 10,
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					>
+						<Text style={{ color: Colors.baseText }}>TIPO DE PRODUCTO</Text>
+					</View>
+					<View style={{ borderColor: Colors.mediumGray, borderWidth: 0.3 }} />
+					<View style={{ justifyContent: 'center', marginTop: 30 }}>
+						<CheckBox
+							style={{ padding: 10 }}
+							onClick={() => {}}
+							isChecked={true}
+							rightText={'Cuentas corrientes'}
+							rightTextStyle={{
+								color: Colors.baseText,
+							}}
+							checkedCheckBoxColor={Colors.main}
+						/>
+						<CheckBox
+							style={{ padding: 10 }}
+							onClick={() => {}}
+							isChecked={true}
+							rightText={'Cajas de ahorro'}
+							rightTextStyle={{
+								color: Colors.baseText,
+							}}
+							checkedCheckBoxColor={Colors.main}
+						/>
+						<CheckBox
+							style={{ padding: 10 }}
+							onClick={() => {}}
+							isChecked={true}
+							rightText={'Tarjetas de credito'}
+							rightTextStyle={{
+								color: Colors.baseText,
+							}}
+							checkedCheckBoxColor={Colors.main}
+						/>
+						<CheckBox
+							style={{ padding: 10 }}
+							onClick={() => {}}
+							isChecked={true}
+							rightText={'Prestamos'}
+							rightTextStyle={{
+								color: Colors.baseText,
+							}}
+							checkedCheckBoxColor={Colors.main}
+						/>
+					</View>
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'center',
+							alignItems: 'center',
+							//height: 80,
+						}}
+					>
+						<View
+							style={{ flex: 1, paddingHorizontal: 20, marginVertical: 20 }}
+						>
+							<TouchableOpacity
+								style={{
+									alignItems: 'center',
+									justifyContent: 'center',
+									borderRadius: 15,
+									height: 44,
+									backgroundColor: transparent,
+									borderColor: Colors.main,
+									borderWidth: 1,
+								}}
+								onPress={() => setModalVisible((prevState) => !prevState)}
+							>
+								<Text style={{ color: Colors.main, padding: 10 }}>ACEPTAR</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</View>
+			</Modal>
 		</View>
 	);
 };
